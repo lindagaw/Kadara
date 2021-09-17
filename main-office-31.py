ddb847940c07c62eb7d5c82c9fe0763311c30e8e
Ashley Gao <gaoye0718@gmail.com>
2021-09-17 12:58:47 -0400

    WIP

--- main-office-31.py
+++ main-office-31.py
@@ -40,13 +40,13 @@ if __name__ == '__main__':
     tgt_data_loader = get_office_31(dataset = 'office-31-webcam', train=True)
     tgt_data_loader_eval = get_office_31(dataset = 'office-31-webcam', train=False)
 
-    print(models.resnet18(pretrained=True))
-    print('-------------------------------')
+    #print(models.resnet18(pretrained=True))
+    #print('-------------------------------')
 
     src_encoder = torch.nn.Sequential(*(list(models.resnet18(pretrained=True).children())[:-1]))
-    src_classifier = torch.nn.Linear(2048, 31)
+    src_classifier = torch.nn.Linear(512, 31)
     tgt_encoder = torch.nn.Sequential(*(list(models.resnet18(pretrained=True).children())[:-1]))
-    tgt_classifier = torch.nn.Linear(2048, 31)
+    tgt_classifier = torch.nn.Linear(512, 31)
     critic = init_model(Discriminator(input_dims=params.d_input_dims,
                                       hidden_dims=params.d_hidden_dims,
                                       output_dims=params.d_output_dims),
